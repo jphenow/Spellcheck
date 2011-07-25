ece36584470bbf535ad9106cf0a154bdd1b24dc4
Jon Phenow <j.phenow@gmail.com>
2011-07-25 11:30:44 -0700

    Last tab to 4 spaces fix.

--- lib.py
+++ lib.py
@@ -1,7 +1,7 @@
 #!/usr/bin.python
-"""	Filename: lib.py
-	Author: Jon Phenow <j.phenow@gmail.com>
-	Description: Simple library of some important functions for spellchecking
+""" Filename: lib.py
+    Author: Jon Phenow <j.phenow@gmail.com>
+    Description: Simple library of some important functions for spellchecking
 """
 
 import re
@@ -9,71 +9,71 @@ import string
 
 """Slice the word at each char so we have a cursor for working edits"""
 def splitter( word ):
-	splits = []
-	for i in range( len( word ) + 1 ):
-		splits.append( ( word[:i], word[i:] ) )
-	return splits
+    splits = []
+    for i in range( len( word ) + 1 ):
+        splits.append( ( word[:i], word[i:] ) )
+    return splits
 
-"""Since we're only worried about vowels check each letter to be a vowel,
-	then create a list of words with every vowel tried
+""" Since we're only worried about vowels check each letter to be a vowel,
+    then create a list of words with every vowel tried
 """
 def replacer( splits ):
-	vowel= "aeiou"
-	replaces = []
-	for a, b in splits:
-		if len(b) > 0 and b[0] in vowel:
-			for c in vowel:
-				replaces.append( a + c + b[1:] )
-	return replaces
+    vowel= "aeiou"
+    replaces = []
+    for a, b in splits:
+        if len(b) > 0 and b[0] in vowel:
+            for c in vowel:
+                replaces.append( a + c + b[1:] )
+    return replaces
 
 """Yes, it actually cycles through a set of words and checks if one is in the dictionary."""
 def known( words ):
-	for word in words:
-		if word in dictionary:
-			return [word]
+    for word in words:
+        if word in dictionary:
+            return [word]
 
-"""Recursive function to grab each acceptable letter and return a list of words that have
-	a certain number of duplicated, acceptable letters.
+""" Recursive function to grab each acceptable letter and return a list of words that have
+    a certain number of duplicated, acceptable letters.
 """
-def record_duplicates( previous,						# Previously checked sequence
-					sequence, 							# To be checked
-					amount, 							# A range for allowable duplicates
-					allowed = string.letters,			# Letters allowed to be duplicated 
-					check_dup = False):					# Necessary for spellcheck speed (not generator)
-	if not sequence:
-		return [previous]
-	solutions = []
-	if check_dup:
-		solutions += record_duplicates( previous + sequence[0][0],
-									sequence[1:],
-									amount,
-									allowed = allowed,
-									check_dup = check_dup )
-	for i in amount:
-		if sequence[0][0] in allowed and ( ( check_dup and sequence[0][1] ) or not check_dup ): 
-			solutions += record_duplicates( previous + sequence[0][0] * i,
-										sequence[1:],
-										amount,
-										allowed=allowed,
-										check_dup = check_dup )
-	return solutions
+def record_duplicates( previous,                  # Previously checked sequence
+                    sequence,                     # To be checked
+                    amount,                       # A range for allowable duplicates
+                    allowed = string.letters,     # Letters allowed to be duplicated 
+                    check_dup = False):	          # Necessary for spellcheck speed (not generator)
+    if not sequence:
+        return [previous]
+    solutions = []
+    if check_dup:
+        solutions += record_duplicates( previous + sequence[0][0],
+                                        sequence[1:],
+                                        amount,
+                                        allowed = allowed,
+                                        check_dup = check_dup )
+    for i in amount:
+        if sequence[0][0] in allowed and ( ( check_dup and sequence[0][1] ) or not check_dup ): 
+            solutions += record_duplicates( previous + sequence[0][0] * i,
+                                            sequence[1:],
+                                            amount,
+                                            allowed=allowed,
+                                            check_dup = check_dup )
+    return solutions
 
 """Remove the punctuation we don't want to deal with and make sure to get words above length 2"""
 def words_only( text ):
-	exclude = set( string.punctuation )
-	no_punctuation = ''.join( ch for ch in text if ch not in exclude )
-	return set( re.findall( '[a-z]{2,}', no_punctuation.lower( ) ) )
+    exclude = set( string.punctuation )
+    no_punctuation = ''.join( ch for ch in text if ch not in exclude )
+    return set( re.findall( '[a-z]{2,}', no_punctuation.lower( ) ) )
 
 """Switches the second element in the parameter to uppercase"""
 def make_caps( splits ):
-	caps = []
-	for a, b in splits:
-		b = b.upper()
-		caps.append( a + b )
-	return caps
+    caps = []
+    for a, b in splits:
+        b = b.upper()
+        caps.append( a + b )
+    return caps
 
 """Retrieve the dictionary being used throughout the library"""
 def get_dictionary( ):
-	return dictionary
+    return dictionary
 
 dictionary = set( words_only( file( '/usr/share/dict/words' ).read( ) ) )
